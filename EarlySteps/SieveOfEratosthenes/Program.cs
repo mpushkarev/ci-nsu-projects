6fd00ff92cdf2fffa689f59ae3d40188c34885a3
Mikhail Pushkarev <96192811+mpushkarev@users.noreply.github.com>
2025-05-21 01:12:43 +0700

    chore: add archived SieveOfEratosthenes project

---
+++ EarlySteps/SieveOfEratosthenes/Program.cs
@@ -0,0 +1,42 @@
+﻿/******************************************************************************\
+*                                                                              *
+*  Author:     Mikhail Pushkarev (CI NSU)                                      *
+*  Created:    Approv. September - December 2023                               *
+*                                                                              *
+*  Description:                                                                *
+*    Basic implementation of the Sieve of Eratosthenes algorithm               *
+*    to find prime numbers up to a given limit.                                *
+*    This was one of the very first projects written during the                *
+*    initial stages of learning C#. May contain naive logic or                 *
+*    areas for improvement.                                                    *
+*                                                                              *
+\******************************************************************************/
+
+namespace SieveOfEratosthenes {
+    internal class Program {
+        static void Main() {
+
+            int maxNum = int.Parse(Console.ReadLine());
+
+            int[] numsList = new int[maxNum - 1];
+
+            for (int num = 2; num <= maxNum; num++) {
+                numsList[num - 2] = num;
+            }
+
+            for (int numId = 0; numId < numsList.Length; numId++) {
+                if (numsList[numId] != -1) {
+                    for (int num = numsList[numId] * 2; num - 2 < numsList.Length; num += numsList[numId]) {
+                        numsList[num - 2] = -1;
+                    }
+                }
+            }
+
+            for (int numId = 0; numId < numsList.Length; numId++) {
+                if (numsList[numId] != -1) {
+                    Console.Write($"{numsList[numId]} ");
+                }
+            }
+        }
+    }
+}
